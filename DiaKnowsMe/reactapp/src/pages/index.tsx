ab871283dfbdc131193a352e017fead163a61e70
Zijia Liu <80761505+abinicorn@users.noreply.github.com>
2023-05-27 12:00:18 +1200

    set up NEXT, MUI, symptom & additional pages

---
+++ DiaKnowsMe/reactapp/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <h1>1234</h1>
+  )
+}
---
+++ DiaKnowsMe/reactapp/src/pages/symptom/index.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import { TextField } from '@mui/material';
+
+
+export default function Symptom() {
+  return (
+
+    <div>
+      <h1>Patient Personal Details</h1>
+
+      
+      <FormControl>
+      <FormLabel id="demo-radio-buttons-group-label">Name</FormLabel>
+      <TextField id="outlined-basic" label="e.g. John Smith" variant="outlined" />
+      <FormLabel id="demo-radio-buttons-group-label">Age</FormLabel>
+      <TextField id="outlined-basic" label="e.g. 25" variant="outlined" />
+
+      <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        defaultValue="female"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel value="female" control={<Radio />} label="Female" />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+      </RadioGroup>
+      <FormLabel id="demo-radio-buttons-group-label">Symptom</FormLabel>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <FormLabel id="demo-radio-buttons-group-label">Additional Details</FormLabel>
+      <TextField
+          id="outlined-multiline-static"
+          multiline
+          rows={4}
+          label="Outlined"
+        />
+
+
+        
+    </FormControl>
+
+
+    </div>
+    
+
+
+  )
+}
+
+
